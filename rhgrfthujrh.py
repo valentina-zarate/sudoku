f02e76e1049d8da525ed2c832929b973e60a9d6d
Valentina <valentina.zarate@sagradoalcal.edu.ar>
2023-09-12 15:10:28 -0300

    sudoku 4

--- rhgrfthujrh.py
+++ rhgrfthujrh.py
@@ -6,3 +6,6 @@
                             self.tabla[col][fila].config(background="red")
                         else:
                             self.tabla[col][fila].config(background="green")'''
+        
+        
+#functools partial, enviando a la función comprobar i y j (columna y fila) como para la función test_button
